332c26f69dc68573e6a61aade777be692262b412
AXEL FERNANDO MACEDO GUIMARAES <axelguimaraes2@gmail.com>
2023-04-01 15:47:57 +0100

    added mongoDB connection

--- app.js
+++ app.js
@@ -38,4 +38,15 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Database connection
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Database connected!'))
+.catch(err => console.error('Error connecting to database:', err));
+
+
 module.exports = app;
