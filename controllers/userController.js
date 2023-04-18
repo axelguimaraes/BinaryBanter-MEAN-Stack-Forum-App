a4d75c9abda3b8d3a44e165af585424c184a3979
AXEL FERNANDO MACEDO GUIMARAES <axelguimaraes2@gmail.com>
2023-04-18 23:29:35 +0100

    added routes; updated controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,84 +1,92 @@
-var mongoose = require('mongoose');
-var User = require('../models/userSchema');
+var mongoose = require("mongoose");
+var User = require("../models/userSchema");
 
 var userController = {};
 
 // Show all users
-userController.showAll = function(req, res) {
-    User.find({}).exec((err, dbusers)=>{
-        if (err) {
-            console.log("Error while reading!");
-            res.redirect('/error')
-        } else {
-            console.log(dbusers);
-            res.render('users/usersList', {users: dbusers});
-        }
+userController.showAll = function (req, res) {
+  User.find({})
+    .then((dbusers) => {
+      console.log(dbusers);
+      res.render("users/usersList", { users: dbusers }); // ERRO
     })
-}
+    .catch((err) => {
+      console.log("Error while reading");
+      res.redirect("/error");
+    });
+};
+
+userController.getAllUsers = function (req, res, next) {
+  User.find(function (err, users) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(users);
+    }
+  });
+};
 
 // Show 1 item by id
-userController.show = function(req, res) {
-    User.findOne({_id:req.params.id}).exec((err, dbuser)=>{
-        if (err) {
-            console.log('Error while reading!');
-            res.redirect('/error');
-        } else {
-            res.render('users/userViewDetails', {user: dbuser})
-        }
+userController.show = function (req, res) {
+  User.findOne({ _id: req.params.id })
+    .then((dbuser) => {
+      res.render("users/userViewDetails", { user: dbuser });
     })
-}
+    .catch((err) => {
+      console.log("Error while reading!");
+      res.redirect("/error");
+    });
+};
 
 // form to create 1 user
-userController.formCreate = function(req, res) {
-    res.render('user/createForm');
-}
+userController.formCreate = function (req, res) {
+  res.render("user/createForm");
+};
 
 // creates 1 user as response to a form POST
-userController.create = function(req,res) {
-    var user = new User(req.body);
-    user.save((err)=>{
-        if (err){
-            console.log('Error while saving');
-            res.redirect('/error');
-        } else {
-            res.redirect('/users');
-        }
-    })
-}
+userController.create = function (req, res) {
+  var user = new User(req.body);
+  user.save((err) => {
+    if (err) {
+      console.log("Error while saving");
+      res.redirect("/error");
+    } else {
+      res.redirect("/users");
+    }
+  });
+};
 
 // shows 1 user to edit
-userController.formEdit = function(req, res) {
-    User.findOne({_id:req.params.id}).exec((err, dbuser)=>{
-        if (err) {
-            console.log('Error reading!');
-            res.redirect('/error');
-        } else {
-            res.render('users/userEditDetails', {user: userdb});
-        }
-    })
-}
+userController.formEdit = function (req, res) {
+  User.findOne({ _id: req.params.id }).then((dbuser) => {
+    res.render("users/userEditDetails", { user: userdb }).catch((err) => {
+      console.log("Error reading!");
+      res.redirect("/error");
+    });
+  });
+};
 
 // edits 1 user as response to a POST edit form
-userController.edit = function(req, res) {
-    User.findByIdAndUpdate(req.body._id, req.body, (err, editedUser)=>{
-        if (err) {
-            console.log('Error while saving');
-            res.redirect('/error')
-        } else {
-            res.redirect('/items/show/'+req.body._id);
-        }
-    })
-}
+userController.edit = function (req, res) {
+  User.findByIdAndUpdate(req.body._id, req.body, (err, editedUser) => {
+    if (err) {
+      console.log("Error while saving");
+      res.redirect("/error");
+    } else {
+      res.redirect("/items/show/" + req.body._id);
+    }
+  });
+};
 
 // deletes 1 user
-userController.delete = function(req, res) {
-    User.remove({_id:req.params.id}).exec((err)=>{
-        if (err) {
-            console.log('Error reading')
-        } else {
-            res.redirect('/users')
-        }
+userController.delete = function (req, res) {
+  User.remove({ _id: req.params.id })
+    .then(() => {
+      res.redirect("/users");
     })
-}
+    .catch((err) => {
+      console.log("Error reading");
+    });
+};
 
 module.exports = userController;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/userController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.showAll);
+router.get('/all', userController.getAllUsers);
+
+router.get('/show/:id', userController.show);
+router.get('/create', userController.formCreate);
+router.post('/create', userController.create);
+router.get('/edit/:id', userController.formEdit);
+router.post('/edit/:id', userController.edit);
+router.get('/delete/:id', userController.delete);
 
 module.exports = router;
